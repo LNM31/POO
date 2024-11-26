0694f18e3432b0b83057a9a73c48d9ff6a95c8e1
LNM31 <167527333+LNM31@users.noreply.github.com>
2024-11-27 00:10:49 +0200

    Update tren.java

--- POO/tren.java
+++ POO/tren.java
@@ -29,19 +29,16 @@ class Tren{
 	    }
 	return false;
     }
-    public void display()
+    public String toString()
     {
+	StringBuilder tmp=new StringBuilder();
 	for(int i=0;i<size;i++)
-	    {
-		System.out.print(v[i].valueTip());
-	    }
-	System.out.println();
+	    tmp.append(v[i]).append(" ");
+	return tmp.toString();
     }
-    
 }
 abstract class Vagon{
     protected int colete;
-    protected String tip;
     public Vagon(int c)
     {
 	colete=Function.minim(c,400);
@@ -49,19 +46,17 @@ abstract class Vagon{
     public int Colete()
     {
 	return colete;
-    }
-    public String valueTip()
-    {
-	return tip;
-    }
-    
+    }  
 }
 class Marfa extends Vagon
 {
     public Marfa(int c)
     {
 	super(c);
-	tip="Marfa ";
+    }
+    public String toString()
+    {
+	return "Marfa";
     }
 }
 abstract class Calatori extends Vagon{
@@ -88,20 +83,25 @@ class CalatoriA extends Calatori{
     public CalatoriA(int c,int co)
     {
 	super(Function.minim(40,c),Function.minim(300,co));
-	tip="CalatoriA ";
+    }
+    public String toString()
+    {
+	return "CalatoriA";
     }
 }
 class CalatoriB extends Calatori{
     public CalatoriB(int c,int co)
     {
 	super(c,co);
-	tip="CalatoriB ";
     }
     public void blocare()
     {
 	System.out.println(capacitate+" S au blocat geamurile");
     }
-    
+    public String toString()
+    {
+	return "CalatoriB";
+    }
 }
 class Main{
     public static void main(String[] args)
@@ -120,8 +120,8 @@ class Main{
 	};//710
 	Tren t1=new Tren(v1);
 	Tren t2=new Tren(v2);
-	t1.display();
-	t2.display();
+	System.out.println(t1);
+	System.out.println(t2);
 	if(t1.equals(t2))
 	    System.out.println("Sunt egale");
 	else
